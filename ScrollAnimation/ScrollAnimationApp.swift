9b720013f82ef7e9b8d254e2de779ac83f0d0a7d
tamaoki <treasure6hunter9cole@gmail.com>
2020-07-30 21:34:29 +0900

    initial commit

---
+++ ScrollAnimation/ScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollAnimationApp.swift
+//  ScrollAnimation
+//
+//  Created by 玉置 on 2020/07/30.
+//
+
+import SwiftUI
+
+@main
+struct ScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
